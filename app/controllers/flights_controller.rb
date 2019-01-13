d8a3aebcfb96c66d9fb4049e2a52ffa7f825eb10
Stan van de Poll <swvandepoll@gmail.com>
2019-01-13 22:09:11 +0100

    Completed Screen 1

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,8 @@
 class FlightsController < ApplicationController
   def index
     @airports_array = Airport.all.map { |airport| [airport.location, airport.id] }
+    @dates = Flight.all.map do |flight| 
+      [flight.departure_time.time_formatted]
+    end.uniq
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,11 @@
 class Flight < ApplicationRecord
   belongs_to :arrival_airport, class_name: "Airport"
   belongs_to :departure_airport, class_name: "Airport"
+
+end
+
+class Time 
+  def time_formatted
+    self.strftime("%m/%d/%Y")
+  end
 end
