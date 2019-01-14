daa344a3f9a261ae64ea020dc6b0d0ec07fc6431
Stan van de Poll <swvandepoll@gmail.com>
2019-01-14 10:42:14 +0100

    Completed Screen 2

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,7 +2,16 @@ class FlightsController < ApplicationController
   def index
     @airports_array = Airport.all.map { |airport| [airport.location, airport.id] }
     @dates = Flight.all.map do |flight| 
-      [flight.departure_time.time_formatted]
+      [flight.departure_time.time_formatted, flight.departure_time]
     end.uniq
+
+    if params[:date] 
+      @results = Flight.all.where("departure_airport_id = ? AND 
+        arrival_airport_id = ? AND departure_time BETWEEN ? AND ?", 
+        params[:from_code], params[:to_code], 
+        params[:date].to_datetime.beginning_of_day,
+        params[:date].to_datetime.end_of_day)
+      @num_tickets = params[:num_tickets]
+    end
   end
 end
